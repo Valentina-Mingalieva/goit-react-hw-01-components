53d7957a3d9178a7f1292b8a8ba749010bffaf20
Valentina Mingalieva <valentina.mingalieva@gmail.com>
2021-10-31 15:34:46 +0100

    task1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import "./App.css";
+import './App.css';
+import Profile from './components/Profile/Profile';
+import user from './components/Profile/user.json';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h2>Task 1</h2>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <h2>Task 2</h2>
+    </div>
+  );
 }
 
 export default App;
