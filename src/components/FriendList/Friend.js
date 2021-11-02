1e9204533cd21803baf9965753db62ce1f0e74c4
Valentina Mingalieva <valentina.mingalieva@gmail.com>
2021-11-02 22:22:56 +0100

    friend

--- src/components/FriendList/Friend.js
+++ src/components/FriendList/Friend.js
@@ -1,29 +1,25 @@
 import PropTypes from 'prop-types';
 import s from './FriendList.module.css';
 
-function Friend({ friend }) {
+function Friend({ id, isOnline, name, avatar }) {
   return (
-    <li key={friend.id} className={s.item}>
-      {friend.isOnline ? (
+    <li key={id} className={s.item}>
+      {isOnline ? (
         <span className={s.online}></span>
       ) : (
         <span className={s.offline}></span>
       )}
-      <img className={s.avatar} src={friend.avatar} alt="foto" width="48" />
-      <p className={s.name}>{friend.name}</p>
+      <img className={s.avatar} src={avatar} alt="foto" width="48" />
+      <p className={s.name}>{name}</p>
     </li>
   );
 }
 
 Friend.propTypes = {
-  friend: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      name: PropTypes.string.isRequired,
-      avatar: PropTypes.string,
-      isOnline: PropTypes.bool.isRequired,
-    }),
-  ),
+  /* id: PropTypes.number.isRequired, */
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string,
+  isOnline: PropTypes.bool.isRequired,
 };
 
 export default Friend;
